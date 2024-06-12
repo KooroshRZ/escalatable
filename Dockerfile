7fadb093265f6cfa6a446acae19732c3d2939926
KooroshRZ <KRyptonZ11337@protonmail.com>
2024-06-12 15:40:26 +0330

    level0 and level1

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM ubuntu:latest
-RUN apt-get update && apt-get install -y openssh-server
+RUN apt-get update && apt-get install -y openssh-server neovim nano gcc sudo
 RUN mkdir /var/run/sshd &&\
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/;s/#Banner none/Banner \/etc\/issue/' /etc/ssh/sshd_config &&\
     echo > /etc/issue
 COPY banner /etc/motd
 COPY entry-point.sh /root/entry-point.sh
 COPY creds.txt /root/creds.txt
+COPY challenges /root/challenges
 EXPOSE 22
 ENTRYPOINT ["/root/entry-point.sh"]
--- entry-point.sh
+++ entry-point.sh
@@ -3,16 +3,36 @@
 users=$(cat /root/creds.txt | cut -d ':' -f1)
 creds=$(cat /root/creds.txt)
 
+let "level_index = 0"
+
 for user in $users; do
+
     deluser $user 2>/dev/null
     rm -rf /home/$user 2>/dev/null
+
     useradd $user -m -s /bin/bash
+    chmod +x /home/$user/
+    cp /root/challenges/level$level_index /home/$user/
+
+    chown $user /home/$user/level$level_index 
+    chmod 500 /home/$user/level$level_index
+    let "level_index = level_index + 1"
+    
 done
 
+# sudo
+echo "user0 ALL=(user1) NOPASSWD: /home/user0/level0" >> /etc/sudoers
+echo "user1 ALL=(user2) NOPASSWD:SETENV: /home/user1/level1" >> /etc/sudoers
+
+
+let "user_index = 0"
 for cred in $creds; do
     echo $cred | chpasswd
+    echo $cred > /home/user${user_index}/cred${user_index}.txt
+    chown user$user_index /home/user${user_index}/cred${user_index}.txt
+    chmod 400 /home/user${user_index}/cred${user_index}.txt
+    let "user_index = user_index + 1"
 done
 
 /usr/sbin/sshd -D
-# service ssh start
 exec "$@"
