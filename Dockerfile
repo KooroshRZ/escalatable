f52b31bec8ffcb6125f337db629b99f780882145
KooroshRZ <koorosh.rajabzadeh@gmail.com>
2024-06-17 23:32:47 +0330

    permissions fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:latest
-RUN apt-get update && apt-get install -y openssh-server neovim nano gcc sudo
+RUN apt-get update && apt-get install -y openssh-server neovim nano sudo gcc
 RUN mkdir /var/run/sshd &&\
     sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/;s/#Banner none/Banner \/etc\/issue/' /etc/ssh/sshd_config &&\
     echo > /etc/issue
--- entry-point.sh
+++ entry-point.sh
@@ -19,10 +19,11 @@ for user in $users; do
 
     useradd $user -m -s /bin/bash
     chmod +x /home/$user/
+    chmod o-x /home/$user/
     cp /root/challenges/level$level_index /home/$user/
 
-    chown $user /home/$user/level$level_index 
-    chmod 550  /home/$user/level$level_index
+    chown root:$user /home/$user/level$level_index 
+    chmod 555  /home/$user/level$level_index
     let "level_index = level_index + 1"
 
     chmod -w /home/$user
@@ -60,8 +61,9 @@ let "user_index = 0"
 for cred in $creds; do
     echo $cred | chpasswd
     echo $cred > /home/user${user_index}/cred${user_index}.txt
-    chown user$user_index /home/user${user_index}/cred${user_index}.txt
-    chmod 400 /home/user${user_index}/cred${user_index}.txt
+    chown root:user$user_index /home/user$user_index/cred$user_index.txt
+    # chown user$user_index /home/user${user_index}/cred${user_index}.txt
+    chmod 440 /home/user${user_index}/cred${user_index}.txt
     let "user_index = user_index + 1"
 done
 
